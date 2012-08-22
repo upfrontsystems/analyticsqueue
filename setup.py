9ce5c88e8a32de514b87031ccc16e20c7f706f11
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-08-22 15:23:02 +0200

    Specify correct README file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='upfront.analyticsqueue',
       version=version,
       description="Upfront Analytics Queue",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
