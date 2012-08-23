22e6c2b1bcfbaa1ab70676da9337dd3c5b77d8fd
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-08-23 11:03:03 +0200

    Add data

--- upfront/analyticsqueue/googlequeue.py
+++ upfront/analyticsqueue/googlequeue.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pyga.requests import Tracker, Page, Session, Visitor
 
 
@@ -5,12 +7,30 @@ class GoogleQueue(object):
 
     @classmethod
     def deliver(cls, entry):
+        now = datetime.utcnow()
+
         # make the call to google analytics
         tracker = Tracker(entry['gacode'], entry['domain'])
+
         visitor = Visitor()
+        visitor.current_visit_time = now
         visitor.ip_address = entry['ip_address'] 
+        visitor.user_agent = entry['user_agent']
+        visitor.locale = entry['locale']
+        # possible other data:
+        #visitor.unique_id = None
+        #visitor.first_visit_time = now
+        #visitor.previous_visit_time = now
+        #visitor.visit_count = 1
+        #visitor.flash_version = None
+        #visitor.java_enabled = None
+        #visitor.screen_colour_depth = None
+        #visitor.screen_resolution = None
+
         session = Session()
+
         # drop the science or maths from the path
         path_elements = ('',) + entry['path'][2:]
         page = Page('/'.join(path_elements))
+
         tracker.track_pageview(page, session, visitor)
