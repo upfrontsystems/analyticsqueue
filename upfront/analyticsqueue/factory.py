c986cb8c2b14886f1a1bbfc39e477d02ae2a7b40
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-08-24 12:39:54 +0200

    Add more config args

--- upfront/analyticsqueue/factory.py
+++ upfront/analyticsqueue/factory.py
@@ -2,7 +2,14 @@ from redis import Redis
 from rq import Queue
 
 
-def get_q(q_name):
+    def __init__(self, 
+                 ):
+def get_q(host='localhost', port=6379,
+          db=0, password=None, socket_timeout=None,
+          connection_pool=None, charset='utf-8',
+          errors='strict', decode_responses=False,
+          unix_socket_path=None, q_name):
+
     connection = Redis()
     q = Queue(name=q_name, connection=connection)
     return q
