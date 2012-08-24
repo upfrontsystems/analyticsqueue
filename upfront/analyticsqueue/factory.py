e5fbfd6a4175cddcfb19c84bc67eacfce0c4a5c7
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-08-24 12:45:40 +0200

    Fix

--- upfront/analyticsqueue/factory.py
+++ upfront/analyticsqueue/factory.py
@@ -2,8 +2,6 @@ from redis import Redis
 from rq import Queue
 
 
-    def __init__(self, 
-                 ):
 def get_q(host='localhost', port=6379,
           db=0, password=None, socket_timeout=None,
           connection_pool=None, charset='utf-8',
